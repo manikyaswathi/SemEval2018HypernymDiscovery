ccedc8bcfb1c72cdfe418ecf1e2300aa4c1e5624
hassa418 <hassa418@d.umn.edu>
2017-12-05 19:23:55 -0600

    bash file for running glove

---
+++ Arshia_phase_2/create_glove_w2v/experiment.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Mandatory argument - path to normlized data file to train a GloVe model
+#Creates a vocabulary-frequency map (vocab.txt) from data using module vocab_count
+#Creates cooccurrence.bin using module cooccur
+#Shuffles cooccurrence.bin using module shuffle and create cooccurrence.shuf.bin
+#Creates vector.txt and vector.bin using glove module   
+
+CORPUS=$1
+VOCAB_FILE=vocab.txt
+COOCCURRENCE_FILE=cooccurrence.bin
+COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
+BUILDDIR=build
+SAVE_FILE=vectors
+VERBOSE=2
+MEMORY=4.0
+VOCAB_MIN_COUNT=5
+VECTOR_SIZE=50
+MAX_ITER=15
+WINDOW_SIZE=15
+BINARY=2
+NUM_THREADS=8
+X_MAX=10
+
+$BUILDDIR/vocab_count -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
+if [[ $? -eq 0 ]]
+  then
+  $BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
+  if [[ $? -eq 0 ]]
+  then
+    $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
+    if [[ $? -eq 0 ]]
+    then
+       $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
+    fi
+  fi
+fi
