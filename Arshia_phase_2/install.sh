7211950fe71b0257219f3db007222865166b517c
hassa418 <hassa418@d.umn.edu>
2017-12-05 21:48:34 -0600

    Add files via upload

---
+++ Arshia_phase_2/install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install python-pip
+sudo apt-get install python-numpy
+sudo apt-get install python-scipy
+sudo apt-get install python-dev
+sudo apt-get install python-wheel
+sudo pip install -U scikit-learn  
+sudo pip install psutil		
+sudo pip install argparse
+sudo pip install --upgrade gensim
+sudo pip install tensorflow	
